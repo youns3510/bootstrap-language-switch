0134e55876dd660137bc4026f6b361593b6f6840
Younes Mahmoud <youns3510@gmail.com>
2020-04-17 17:12:54 +0200

    update with preloader

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,8 @@
-$(document).ready(function () {
+$(document).ready(function(){
     $('#tolang-en').on('click touchend', function () {
         translang('en');
     })
-
+    
     $('#tolang-cn').on('click touchend', function () {
         translang('cn');
     })
@@ -14,7 +14,7 @@ function translang(tolang) {
     if (tolang == 'en') {
         console.log(' to english')
         if (url.search("googtrans\/en\/zh-CN") != -1) {
-            url = url.replace('\/cn\/','/en/');
+            url = url.replace('\/cn\/', '/en/');
             url = url.replace("googtrans\/en\/zh-CN", 'googtrans/en/en');
             console.log('to url  ' + url);
         }
@@ -23,12 +23,12 @@ function translang(tolang) {
     if (tolang == 'cn') {
         console.log(' to chinese')
         if (url.search("googtrans\/en\/en") != -1) {
-            url = url.replace('\/en\/','/cn/');
+            url = url.replace('\/en\/', '/cn/');
             url = url.replace("googtrans\/en\/en", 'googtrans/en/zh-CN')
             console.log('to url ' + url)
-        }     
+        }
     }
 
     window.location.href = url;
-            // location.reload(true);
+    // location.reload(true);
 }
